d00eaeff33f7e8c85b9e3467a23a88b384108d12
maxvollmer <maxvollmer@users.noreply.github.com>
2019-07-15 01:32:15 +0200

    Implemented hook function that replaces engine's Mod_Extradata function with custom implementation in client.dll. Prevents engine from killing the game with those annoying "Mod_Extradata: caching failed" error messages. Instead we print a warning to the debug console and carry on.

---
+++ cl_dll/VRModExtraDataCacheHook.cpp
@@ -0,0 +1,46 @@
+
+#include <string>
+#include <iostream>
+#include <Windows.h>
+
+#include <easyhook.h>
+
+#include "hud.h"
+#include "cl_util.h"
+#include "const.h"
+#include "com_model.h"
+#include "studio.h"
+#include "cl_entity.h"
+#include "r_studioint.h"
+
+extern engine_studio_api_t IEngineStudio;
+
+void* MyModExtradata(struct model_s *mod)
+{
+	extern studiohdr_t* Mod_Extradata(const char* callerInfo, cl_entity_t* ent, model_t* mod);
+	return Mod_Extradata("EngineHook", nullptr, mod);
+}
+
+void InstallModExtraDataCacheHook()
+{
+	HOOK_TRACE_INFO hHook = { NULL };
+
+	NTSTATUS result = LhInstallHook(
+		IEngineStudio.DONOTUSEMod_ExtradataDONOTUSE,
+		MyModExtradata,
+		NULL,
+		&hHook);
+
+	if (FAILED(result))
+	{
+		std::wstring ws(RtlGetLastErrorString());
+		std::string s{ ws.begin(), ws.end() };
+		gEngfuncs.Con_DPrintf("Warning: Failed to install Mod_Extradata hook: %s\n", s.c_str());
+	}
+	else
+	{
+		ULONG ACLEntries[1] = { 0 };
+		LhSetInclusiveACL(ACLEntries, 1, &hHook);
+		//gEngfuncs.Con_DPrintf("Successfully installed hook!\n");
+	}
+}
