ab70625e8247bc86faee5122db0b95ba065f2473
maxvollmer <maxvollmer@users.noreply.github.com>
2020-02-20 19:21:22 +0100

    Improved installer interface and added uninstall option

--- HLVRInstaller/MainWindow.xaml.cs
+++ HLVRInstaller/MainWindow.xaml.cs
@@ -21,14 +21,19 @@ namespace HLVRInstaller
             InitializeComponent();
             HalflifeDirectoryFinder.InitializeFromSteam();
             HalfLifeDirectoryInput.Text = HalflifeDirectoryFinder.HLDirectory;
-            HalfLifeDirectoryInput.TextChanged += (object sender, TextChangedEventArgs e) => { HalflifeDirectoryFinder.HLDirectory = HalfLifeDirectoryInput.Text; };
+            HalfLifeDirectoryInput.TextChanged += (object sender, TextChangedEventArgs e) => 
+            { 
+                HalflifeDirectoryFinder.HLDirectory = HalfLifeDirectoryInput.Text;
+                UninstallButton.Visibility = HalflifeDirectoryFinder.CheckModDirectory() ? Visibility.Visible : Visibility.Collapsed;
+            };
             using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("HLVRInstaller.README.txt"), Encoding.UTF8))
             {
                 ReadmeContent.Inlines.Add(new Run(reader.ReadToEnd()));
             }
+            UninstallButton.Visibility = HalflifeDirectoryFinder.CheckModDirectory() ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        private void BrowseHalfLifeDirectory(object sender, RoutedEventArgs e)
+        private void BrowseHalfLifeDirectory(object sender, RoutedEventArgs args)
         {
             using (var folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog() { SelectedPath = HalflifeDirectoryFinder.HLDirectory })
             {
@@ -40,32 +45,58 @@ namespace HLVRInstaller
             }
         }
 
-        private void Quit(object sender, RoutedEventArgs e)
+        private void Quit(object sender, RoutedEventArgs args)
         {
             Application.Current.Shutdown();
         }
 
-        private void CancelInstallation(object sender, RoutedEventArgs e)
+        private void CancelInstallation(object sender, RoutedEventArgs args)
         {
-            var result = MessageBox.Show("Are you sure you want to cancel installation? Files might be in a weird state if you do. (This installer is way too dumb to be able to undo a half-completed installation.)", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
-            if (result == MessageBoxResult.OK)
+            var result = MessageBox.Show(this, "Are you sure you want to cancel installation? Files might be in a weird state if you do. (This installer is way too dumb to be able to undo a half-completed installation.)", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+            if (result == MessageBoxResult.Yes)
             {
                 doCancel = true;
             }
         }
 
-        private async void InstallHalfLife(object sender, RoutedEventArgs e)
+        private void UninstallMod(object sender, RoutedEventArgs args)
+        {
+            if (!HalflifeDirectoryFinder.CheckModDirectory())
+            {
+                UninstallButton.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            var result = MessageBox.Show(this, "Are you sure you want to uninstall Half-Life: VR? All files, including savegames and your settings, will be removed.", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Directory.Delete(HalflifeDirectoryFinder.VRDirectory, true);
+
+                    MessageBox.Show(this, "Successfully uninstalled Half-Life: VR.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(this, "Error while trying to uninstall Half-Life: VR: " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            UninstallButton.Visibility = HalflifeDirectoryFinder.CheckModDirectory() ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private async void InstallMod(object sender, RoutedEventArgs args)
         {
             if (!HalflifeDirectoryFinder.CheckHLDirectory())
             {
-                var result = MessageBox.Show("\"" + HalflifeDirectoryFinder.HLDirectory + "\" doesn't seem to be a valid Half-Life installation. Install anyways?", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+                var result = MessageBox.Show(this, "\"" + HalflifeDirectoryFinder.HLDirectory + "\" doesn't seem to be a valid Half-Life installation. Install anyways?", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
                 if (result == MessageBoxResult.Cancel)
                     return;
             }
 
             if (HalflifeDirectoryFinder.CheckModDirectory())
             {
-                var result = MessageBox.Show("Half-Life: VR is already installed. Install anyways? (All files will be overwritten.)", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+                var result = MessageBox.Show(this, "Half-Life: VR is already installed. Install anyways? (All files will be overwritten.)", "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
                 if (result == MessageBoxResult.Cancel)
                     return;
             }
@@ -91,14 +122,14 @@ namespace HLVRInstaller
 
                 if (!string.IsNullOrEmpty(InstallError))
                 {
-                    MessageBox.Show(InstallError, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(this, InstallError, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                     return;
                 }
 
                 if (HalflifeDirectoryFinder.CheckModDirectory())
                 {
-                    var result = MessageBox.Show("Installation succeeded! Press OK to launch Half-Life: VR, or Cancel to quit.", "Yay", MessageBoxButton.OKCancel, MessageBoxImage.Information);
-                    if (result == MessageBoxResult.OK)
+                    var result = MessageBox.Show(this, "Installation succeeded! Installer will quit. Do you want to launch Half-Life: VR now?", "Yay", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                    if (result == MessageBoxResult.Yes)
                     {
                         Process.Start(new ProcessStartInfo
                         {
@@ -111,13 +142,14 @@ namespace HLVRInstaller
                 }
                 else
                 {
-                    MessageBox.Show("Installation failed. Not really sure why :/", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(this, "Installation failed. Not really sure why :/", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
             finally
             {
                 PreInstall.Visibility = Visibility.Visible;
                 DuringInstall.Visibility = Visibility.Collapsed;
+                UninstallButton.Visibility = HalflifeDirectoryFinder.CheckModDirectory() ? Visibility.Visible : Visibility.Collapsed;
             }
         }
 
