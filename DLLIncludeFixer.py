bdaacaaae819663c7cdac17f7b6ec6639f26a298
maxvollmer <maxvollmer@users.noreply.github.com>
2022-05-30 22:06:09 +0200

    Small changes for Python3 and VS2022

--- DLLIncludeFixer.py
+++ DLLIncludeFixer.py
@@ -11,14 +11,14 @@ def main(argv):
 			if opt in ("-i", "--ifile"):
 				inputfile = arg
 	except getopt.GetoptError:
-		print 'DLLIncludeFixer.py -i <inputfile>'
+		print('DLLIncludeFixer.py -i <inputfile>')
 		sys.exit(2)
 
 	if not os.path.exists(inputfile):
-		print 'DLLIncludeFixer.py -i <inputfile>'
+		print('DLLIncludeFixer.py -i <inputfile>')
 		sys.exit(2)
 
-	print 'Modifying DLL includes in ', inputfile
+	print('Modifying DLL includes in ' + inputfile)
 
 	f = open(inputfile, 'rb')
 	s = f.read()
