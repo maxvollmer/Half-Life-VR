942fc744d37e1efcefb04db615039e1496502623
maxvollmer <maxvollmer@users.noreply.github.com>
2022-09-15 04:13:34 +0200

    Added script that automagically creates a Half-Life: VR folder from Half-Life

--- docs/dev-guide.md
+++ docs/dev-guide.md
@@ -28,6 +28,7 @@ All source code lives in [src](../src).
 
 1. Open [shared.props](../src/shared.props) and make sure `HLVRInstallDirectory` points to the directory where you have Half-Life: VR installed.
     - If you don't have Half-Life: VR installed, see [Create a Half-Life: VR folder from Half-Life](#create-a-half-life-vr-folder-from-half-life) below.
+    - Visual Studio will automagically copy the generated files into your Half-Life: VR installation. The debugger will also correctly launch Half-Life: VR out of the box.
 
 2. Open [HLVR.sln](../src/HLVR.sln) with Visual Studio.
 
@@ -40,7 +41,23 @@ If there are issues or if you have questions, join my Discord Server [Max Makes 
 ---
 #### Create a Half-Life: VR folder from Half-Life
 
-Half-Life: VR is a heavily modified version of Half-Life. If you don't have Half-Life: VR installed, e.g. if you are following this guide before the Steam release, you can create your own Half-Life: VR folder using the following instructions:
+Half-Life: VR is a heavily modified version of Half-Life. Building the source in this repo creates `client.dll`, `hl.dll`, and `HLVRConfig.exe`. For the full mod you still need several original game files as well as certain game files modified for VR.
+
+If you don't have Half-Life: VR installed, e.g. if you are following this guide before the Steam release, you need to create your own Half-Life: VR folder. For this you need a valid installation of Half-Life. You can create the Half-Life: VR folder manually, or you can use a script provided in this repository.
+
+##### 1. Using the CreateHLVRFromHL.py script
+
+  - Run [CreateHLVRFromHL.py](../src/CreateHLVRFromHL.py) and provide the path to your Half-Life installation like so:  
+    `python CreateHLVRFromHL.py -h "Path/To/SteamLibrary/steamapps/common/Half-Life"`
+  - The script will create a new `Half-Life VR` folder in the same location where your `Half-Life` folder is.
+  - Some things to consider:
+    - You must run the script from the `src` folder in a complete clone of this repo.
+    - Your Half-Life installation must be a valid installation from Steam.
+    - No `Half-Life VR` folder must exist yet.
+    - You must have NuGet packages for `HLVRConfig` installed.
+  - If the script encounters any problems, it will output an error message with a detailed explanation.
+
+##### 2. Creating the folder manually
 
 1. Create a copy of your `Half-Life` folder and name it `Half-Life VR`.
 
@@ -78,6 +95,9 @@ Half-Life: VR is a heavily modified version of Half-Life. If you don't have Half
       - MarkdownSharp.dll
       - HtmlToXamlConverter.dll
 
-5. Now follow the [Build The Mod](#build-the-mod) instructions above to build `client.dll`, `hl.dll`, and `HLVRConfig.exe`.
+5. If it exists, delete the folder `valve_hd`.
 
+Now follow the [Build The Mod](#build-the-mod) instructions above to build `client.dll`, `hl.dll`, and `HLVRConfig.exe`.
+
+---
 That should be it. If you run into problems or have questions, join my Discord Server [Max Makes Mods](https://discord.gg/jujwEGf62K)!
---
+++ src/CreateHLVRFromHL.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+
+# This script creates a new Half-Life: VR directory from a vanilla Half-Life installation.
+# Must be run from the 'src' directory within a freshly (and completely) cloned Half-Life: VR repository (https://github.com/maxvollmer/Half-Life-VR).
+# Run like so: CreateHLVRFromHL.py -h <hlpath>
+# where <hlpath> is the full path to your Half-Life installation.
+# NOTE: This just prepares the folder.
+# NOTE: You still need to build client.dll, hl.dll, and HLVRConfig.exe for the mod do actually work.
+
+# THIS SCRIPT IS FOR DEVELOPERS WHO WANT TO BUILD THE MOD
+# IT IS NOT INTENDED FOR PLAYERS
+# USE AT OWN RISK
+
+import sys, getopt, os, shutil, pathlib
+
+dlls_to_copy = [
+"packages/Microsoft.Experimental.Collections.1.0.6-e190117-3/lib/netstandard2.0/Microsoft.Experimental.Collections.dll",
+"packages/MarkdownSharp.2.0.5/lib/net40/MarkdownSharp.dll",
+"packages/Newtonsoft.Json.13.0.1/lib/net45/Newtonsoft.Json.dll",
+"packages/HtmlToXamlConverter.1.0.5727.24510/lib/net45/HtmlToXamlConverter.dll",
+
+"cegui/bin/CEGUIBase-0.dll",
+"cegui/bin/CEGUICommonDialogs-0.dll",
+"cegui/bin/CEGUICoreWindowRendererSet.dll",
+"cegui/bin/CEGUIExpatParser.dll",
+"cegui/bin/CEGUIOpenGLRenderer-0.dll",
+"cegui/bin/CEGUISILLYImageCodec.dll",
+"cegui/bin/freetype.dll",
+"cegui/bin/glew.dll",
+"cegui/bin/glfw.dll",
+"cegui/bin/jpeg.dll",
+"cegui/bin/libpng.dll",
+"cegui/bin/pcre.dll",
+"cegui/bin/SILLY.dll",
+"cegui/bin/zlib.dll",
+
+"cl_dll/openvr/openvr_api.dll",
+"cl_dll/EasyHook/bin/EasyHook32.dll",
+"cl_dll/fmod/lib/x86/fmod.dll",
+"cl_dll/fmod/lib/x86/fmodL.dll"
+]
+
+folders_to_copy = [
+'../art/sound',
+'../art/textures',
+'../art/sprites',
+'../art/models',
+
+'../game/actions',
+'../game/resource',
+]
+
+def main(argv):
+    script_location = os.path.dirname(pathlib.Path(__file__).absolute())
+
+    if not os.path.basename(script_location) == "src" or not os.path.exists(os.path.join(script_location, 'HLVR.sln')):
+        print('Error: Unexpected script location. Script must be run from the "src" folder in a fully checked out Half-Life: VR repository (https://github.com/maxvollmer/Half-Life-VR).')
+        sys.exit(2)
+
+    if not os.path.exists(os.path.join(script_location, 'packages')):
+        print('Error: "packages" folder not found in "src" folder. Please install all NuGet packages required by HLVRConfig before continuing.')
+        sys.exit(2)
+
+    for dll in dlls_to_copy:
+        if not os.path.exists(os.path.join(script_location, dll)):
+            print('Error: Library "'+dll+'" not found in repository. Make sure you have a fully checked out Half-Life: VR repository (https://github.com/maxvollmer/Half-Life-VR), AND ensure you have installed all NuGet packages required by HLVRConfig.')
+            sys.exit(2)
+
+    for folder in folders_to_copy:
+        if not os.path.exists(os.path.join(script_location, folder)):
+            print('Error: Folder "'+folder+'" not found in repository. Make sure you have a fully checked out Half-Life: VR repository (https://github.com/maxvollmer/Half-Life-VR).')
+            sys.exit(2)
+
+    hlpath = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "h:",["hlpath="])
+        for opt, arg in opts:
+            if opt in ("-h", "--hlpath"):
+                hlpath = arg
+    except getopt.GetoptError:
+        print('Error: Please provide a valid path to Half-Life like so: "CreateHLVRFromHL.py -h <hlpath>"')
+        sys.exit(2)
+
+    if not os.path.exists(hlpath):
+        print('Error: Path doesn\'t exist. Please provide a valid path to Half-Life like so: "CreateHLVRFromHL.py -h <hlpath>"')
+        sys.exit(2)
+
+    if not os.path.basename(hlpath) == "Half-Life":
+        print('Error: Expected folder name "Half-Life", "'+os.path.basename(hlpath)+'" found instead. Please provide a valid path to Half-Life like so: "CreateHLVRFromHL.py -h <hlpath>"')
+        sys.exit(2)
+
+    if not os.path.exists(os.path.join(hlpath,"hl.exe")):
+        print('Error: No hl.exe found in Half-Life folder. Please provide a valid path to Half-Life like so: "CreateHLVRFromHL.py -h <hlpath>"')
+        sys.exit(2)
+
+    hlvrpath = hlpath + ' VR'
+
+    if os.path.exists(hlvrpath):
+        print('Error: "' + hlvrpath + '" already exists, aborting.')
+        sys.exit(2)
+
+    print('Creating Half-Life: VR folder at "' + hlvrpath + '" from Half-Life folder at "' + hlpath + '". Please wait, this can take a while.')
+
+    shutil.copytree(hlpath, hlvrpath)
+
+    for dll in dlls_to_copy:
+        print('Copying "'+os.path.join(script_location, dll)+'".')
+        shutil.copy(os.path.join(script_location, dll), hlvrpath)
+
+    for folder in folders_to_copy:
+        print('Copying "'+os.path.join(script_location, folder)+'" to "'+os.path.join(hlvrpath, 'valve', os.path.basename(folder))+'".')
+        shutil.copytree(os.path.join(script_location, folder), os.path.join(hlvrpath, 'valve', os.path.basename(folder)), dirs_exist_ok=True)
+
+    if os.path.exists(os.path.join(hlvrpath, 'valve_hd')):
+        print('Removing obsolete folder "valve_hd".')
+        shutil.rmtree(os.path.join(hlvrpath, 'valve_hd'))
+
+    shutil.copy(os.path.join(script_location, '../art/game_icon.ico'), os.path.join(hlvrpath, 'valve', 'game.ico'))
+
+    print('All done! New Half-Life: VR folder created at "'+hlvrpath+'". NOTE: You still need to build client.dll, hl.dll, and HLVRConfig.exe for the mod to work.')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
